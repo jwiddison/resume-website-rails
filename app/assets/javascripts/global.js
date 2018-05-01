668825143da74809bf0beefffc0e22abd7353e94
Jordan Widdison <jordan.widdison@gmail.com>
2018-04-30 20:25:28 -0600

    Get a lot of things working

---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,33 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({target: '.navbar-fixed-top', offset: 51});
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+  });
+
+  // Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 100
+    }
+  })
+
+})(jQuery); // End of use strict
+
+document.getElementById('id_phone').addEventListener('input', function(e) {
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+  e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3]? '-' + x[3]: '');
+});
